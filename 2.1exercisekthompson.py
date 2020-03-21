d435aaf09fd5d6494a98be27b1c63d973bdec3f8
kaymaethomp1 <54689933+kaymaethomp1@users.noreply.github.com>
2020-03-21 12:19:28 -0500

    Add files via upload

---
+++ 2.1exercisekthompson.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Course: DSC530
+2.1 Exercise
+Date: 03/21/2020
+Name: Kayla Thompson
+Description: This code completes various basic tasks in python. 
+"""
+
+print('Hello World! I wonder why that is always the default coding text to start with?')
+
+add = 2+2
+print(add)
+
+sub = 4-2
+print(sub)
+
+mult = 4*2
+print(mult)
+
+divide = 4/2
+print(divide)
+
+word1 = 'Go '
+word2 = 'Bucks'
+print(word1 + word2)
+
+my_list = ["Mom", "Dad", "Aunt", "Uncle"]
+print(my_list)
+my_list.append("Sister")
+print(my_list)
+
+my_tuple = ("red", "orange", "yellow", "green")
+print(my_tuple)
+
